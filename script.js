a1763af2cb371ffa322d8d46e3fafb87cb2a2d71
sarahDohOnline <info@sarahdohertyonline.co.uk>
2022-09-20 01:10:43 +0100

    Fixed nav-scroll
    altered navigation to hide nav on scroll

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0";
+  } else {
+    document.getElementById("nav-bar").style.top = "-120px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
@@ -13,13 +25,3 @@ navItems.forEach(listItem => {
   listItem.addEventListener("click", setActive)
 })
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("nav-bar").style.top = "0";
-//   } else {
-//     document.getElementById("nav-bar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
