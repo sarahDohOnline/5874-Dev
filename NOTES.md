244ec6a3bc33598acecf0ec802e62aa6a1afcd72
sarahDohOnline <info@sarahdohertyonline.co.uk>
2022-09-21 09:56:04 +0100

    More functionality
    Adding more functionality;
    Reviews- JSON
    added portfolio grid image

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+Challanges
+
+Testimonials
+   - Tight for time but I wanted to show my ability and understanding of APIs more than design.
+   - Not an active slider menu - loading the json into cards was ok, but then adding the slider element was where I struggled. I will figure it out!! but I didn't have time before Thursday.
+
+
+Overalll technical understanding of the project. This technical task made me value teamwork and collaboration, as it helped me identify areas for improvemnent that I would easly improve on in a team senario.
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-
+// Mobile Menu Toggle functionality
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -18,10 +18,55 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+//filter grid
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.itemBox');
 
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
 
+        let dataFilter = this.getAttribute('data-filter');
 
-navItems.forEach(listItem => {
-  listItem.addEventListener("click", setActive)
-})
-
+        for(let k = 0; k<itemBox.length; k++){
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+        
+        if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itemBox[k].classList.remove('hide');
+            itemBox[k].classList.add('active');
+        }
+    }
+    })
+}
+let http = new XMLHttpRequest();
+// sets the http method - GET .- pass json file - async.
+http.open('get', 'review.json', true);
+//server send file
+http.send();
+//catch response
+http.onload = function(){
+  //check for sucsessful response
+   if(this.readyState == 4 && this.status == 200){
+    //convert to JS object
+      let reviews = JSON.parse(this.responseText);
+      //empty vairable
+      let output = "";
+      //loopthrough reviews 
+      for(let item of reviews){
+         output += `
+            <div class="review">
+               <p class="message">${item.message}</p>
+               <img src="${item.image}" alt="${item.userName}">
+               <p class="userName">${item.userName}</p>
+               <p class="title">${item.title}</p>
+            </div>
+         `;
+      }
+      //target container add the data that output variable holds.
+      document.querySelector(".reviews").innerHTML = output;
+   }
+}
